9af9a78cbd8d5ea1ad95d94fb3a91b7dba94a424
devojeda <172947644+DevOjeda016@users.noreply.github.com>
2025-12-29 12:27:09 -0600

    feat: implement orders module with create, find, and status change functionalities

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,30 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { OrdersService } from './orders.service';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern({ cmd: 'create_order' })
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern({ cmd: 'find_all_orders' })
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'find_one_order' })
+  findOne(@Payload() id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'change_order_statsu' })
+  changeOrderStatus(@Payload() data: { id: number; status: string }) {
+    const { id, status } = data;
+    return this.ordersService.changeOrderStatus(id, status);
+  }
+}

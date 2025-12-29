263da096af528e1ff3d4817398503f328248b1b7
devojeda <172947644+DevOjeda016@users.noreply.github.com>
2025-12-29 12:16:18 -0600

    feat: add NestJS microservices support and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('MSOrders');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
 
-  await app.listen(envs.PORT);
+  await app.listen();
   logger.log(`Microservice is running on port ${envs.PORT}`);
 }
 
